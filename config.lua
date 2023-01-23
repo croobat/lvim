8dc91a0df809630b198a80f74c915795a6978a07
Tony Ramírez <tonyramirez.business@outlook.com>
2023-01-23 13:49:15 -0600

    rearrange main

--- config.lua
+++ config.lua
@@ -3,35 +3,20 @@ reload("impatient")
 --  ┌──────┐
 --  │ Core │
 --  └──────┘
-reload("tony.plugins")
 reload("tony.options")
 reload("tony.keymaps")
 reload("tony.autocommands")
 reload("tony.lsp")
-
---  ┌───────────────┐
---  │ Extra plugins │
---  └───────────────┘
-reload("tony.plugin.whichkey")
-reload("tony.plugin.commentbox")
-reload("tony.plugin.vimwiki")
-
---  ┌───────┐
---  │ Theme │
---  └───────┘
--- lvim.colorscheme = "tokyonight"
-require("tokyonight").setup({
-	style = "night",
-	on_colors = function(colors)
-		colors.comment = "#626c9c"
-	end,
-})
 
 --  ┌──────────────┐
 --  │ Lvim builtin │
 --  └──────────────┘
 lvim.log.level = "info"
 lvim.format_on_save = { enabled = false, pattern = "*.lua", timeout = 1000 }
+
+-- Theme
+-- lvim.colorscheme = "tokyonight"
+require("tokyonight").setup({ style = "night", on_colors = function(colors) colors.comment = "#626c9c" end, })
 
 -- Alpha
 lvim.builtin.alpha.active = false
@@ -119,3 +104,108 @@ lvim.builtin.treesitter.ensure_installed = "all"
 lvim.builtin.treesitter.autotag.enable = true
 lvim.builtin.treesitter.rainbow.enable = true
 lvim.builtin.treesitter.rainbow.colors = { "#bb9af7", "#e0af68", "#2ac3de", "#9ece6a", "#f7768e" }
+
+-- Whichkey
+reload("tony.plugin.whichkey")
+
+--  ┌──────────────────┐
+--  │ External Plugins │
+--  └──────────────────┘
+lvim.plugins = { -- {{{
+	{ "lewis6991/impatient.nvim" },
+	-- Syntax
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{ "JoosepAlviste/nvim-ts-context-commentstring", event = "BufRead" },
+	{ "p00f/nvim-ts-rainbow" },
+	{
+		"romgrk/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true,
+				throttle = true,
+				max_lines = 0,
+				patterns = { default = { "class", "function", "method" } },
+			})
+		end,
+	},
+	{ "NMAC427/guess-indent.nvim", config = "require('guess-indent').setup{}" },
+	{ "neoclide/vim-jsx-improve", ft = { "javascript", "javascriptreact" } },
+	{ "joshua7v/vim-tsx-improve", ft = { "typescript", "typescriptreact" } },
+
+	-- Git
+	{ "sindrets/diffview.nvim", event = "BufRead" },
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"G",
+			"Git",
+			"Gdiffsplit",
+			"Gread",
+			"Gwrite",
+			"Ggrep",
+			"GMove",
+			"GDelete",
+			"GBrowse",
+			"GRemove",
+			"GRename",
+			"Glgrep",
+			"Gedit",
+		},
+		ft = { "fugitive" },
+	},
+
+	-- Debug
+	{ "folke/trouble.nvim", cmd = "TroubleToggle" },
+	{ "mxsdev/nvim-dap-vscode-js", dependencies = { "mfussenegger/nvim-dap" } },
+
+	-- Visuals
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+				RGB = true,
+				RRGGBB = true,
+				RRGGBBAA = true,
+				rgb_fn = true,
+				hsl_fn = true,
+				css = true,
+				css_fn = true,
+			})
+		end,
+	},
+
+	-- Preview files
+	{ "npxbr/glow.nvim", ft = { "markdown" } },
+	{
+		"turbio/bracey.vim",
+		cmd = { "Bracey", "BracyStop", "BraceyReload", "BraceyEval" },
+		build = "npm install --prefix server",
+	},
+
+	-- Vanilla
+	{ "tpope/vim-repeat" },
+	{ "tpope/vim-surround" },
+	{ "felipec/vim-sanegx", event = "BufRead" },
+	{ "wellle/targets.vim" },
+
+	-- Misc
+	{
+		"folke/todo-comments.nvim",
+		event = "BufRead",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{ "LudoPinelli/comment-box.nvim", config = "require 'plugins.comment-box'" },
+	{ "vimwiki/vimwiki", config = "require 'plugins.vimwiki'", ft = { "vimwiki", "markdown" } },
+	{ "folke/zen-mode.nvim", cmd = { "ZenMode" } },
+	{ "lambdalisue/suda.vim", lazy = false },
+} -- }}}
+
+reload("tony.plugin.commentbox")
+reload("tony.plugin.vimwiki")
