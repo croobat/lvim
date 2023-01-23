d5dcec27a3df6ac647776b127a6c196433efcbd0
Tony Ramírez <tonyramirez.business@outlook.com>
2023-01-23 17:57:17 -0600

    add rust plugins

--- config.lua
+++ config.lua
@@ -163,6 +163,20 @@ lvim.plugins = { -- {{{
 	{ "folke/trouble.nvim", cmd = "TroubleToggle" },
 	{ "mxsdev/nvim-dap-vscode-js", dependencies = { "mfussenegger/nvim-dap" } },
 
+	-- Rust
+	"simrat39/rust-tools.nvim",
+	{ "saecki/crates.nvim", version = "v0.3.0", dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup({
+				null_ls = { enabled = true, name = "crates.nvim", },
+				popup = { border = "rounded", },
+			})
+		end, },
+	{ "j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup()
+		end, },
+
 	-- Visuals
 	{
 		"norcalli/nvim-colorizer.lua",
