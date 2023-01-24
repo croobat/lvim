c22ba5ac54b9221f4a77acfb9017a3d1c4586196
Tony Ramírez <tonyramirez.business@outlook.com>
2023-01-24 09:20:50 -0600

    move vimwiki configs to markdown ftplugin

--- lua/tony/autocommands.lua
+++ lua/tony/autocommands.lua
@@ -80,14 +80,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	callback = function()
 		vim.opt_local.wrap = true
 		vim.opt_local.spell = true
-		vim.cmd("let g:vimwiki_list = [{'path': '~/Documents/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]")
-		-- vim.cmd("let g:vimwiki_key_mappings = { 'all_maps': 0, 'global': 0, }")
-		vim.cmd("hi VimwikiHeader2 guifg=#ffb86c gui=bold")
-		vim.cmd("hi VimwikiHeader3 guifg=#ff79c6 gui=bold")
-		vim.cmd("hi VimwikiHeader4 guifg=#50fa7b gui=bold")
-		vim.cmd("hi VimwikiHeader5 guifg=#f1fa8c gui=italic")
-		vim.cmd("hi VimwikiHeader6 guifg=#f1fa8c")
-		vim.cmd("hi VimwikiLink    guifg=#8BE9FD gui=italic")
 	end,
 })
 
